9447b1a5f6ff2f7162b6f96b29d1889a35ec15fd
xxh200812345 <46435236+xxh200812345@users.noreply.github.com>
2021-04-15 16:03:29 +0900

    Update importfile.js

--- js/importfile.js
+++ js/importfile.js
@@ -1,44 +1,79 @@
-LIB＿PATH = '';
+//local net
+const LIB＿PATH = './com/lib';
 
-BOOTSTRAP_JS_PATH = LIB＿PATH + 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js';
-BOOTSTRAP_CSS_PATH = LIB＿PATH + 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css';
+const BOOTSTRAP_JS_PATH = LIB＿PATH + '/bootstrap/js/bootstrap.bundle.min.js';
+const BOOTSTRAP_CSS_PATH = LIB＿PATH + '/bootstrap/css/bootstrap.min.css';
 
-JQ_PATH = LIB＿PATH + 'https://code.jquery.com/jquery-3.6.0.min.js';
+const JQ_PATH = LIB＿PATH + '/jquery-3.5.1.min.js';
 
-JQ_UI_JS_PATH = LIB＿PATH + 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js';
-JQ_UI_CSS_PATH = LIB＿PATH + 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css';
+const JQ_UI_JS_PATH = LIB＿PATH + '/jquery-ui.min.js';
+const JQ_UI_CSS_PATH = LIB＿PATH + '/jquery-ui.min.css';
 
-VUEJS_PATH = LIB＿PATH + 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js';
-CUSTOM_VUEJS_PATH = './js/dovue.js';
+const VUEJS_PATH = LIB＿PATH + '/vue.js';
+
+//online
+const W_BOOTSTRAP_JS_PATH = 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js';
+const W_BOOTSTRAP_CSS_PATH = 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css';
+
+const W_JQ_PATH = 'https://code.jquery.com/jquery-3.6.0.min.js';
+
+const W_JQ_UI_JS_PATH = 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js';
+const W_JQ_UI_CSS_PATH = 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css';
+
+const W_VUEJS_PATH = 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js';
+
+//主逻辑文件
+const CUSTOM_VUEJS_PATH = './js/dovue.js';
+
 
 //console.log = () => { };
 
 window.onload = function () {
 
 	let jqEl = appendJs(JQ_PATH);
-	console.log(JQ_PATH);
 
-	//等待jqurey加载完毕
 	jqEl.onload = () => {
-		$(function () {
-			appendJs(BOOTSTRAP_JS_PATH);
-			appendJs(JQ_UI_JS_PATH);
-			let vuejs = appendJs(VUEJS_PATH);
 
-			appendCss(BOOTSTRAP_CSS_PATH);
-			appendCss(JQ_UI_CSS_PATH);
+		console.log("[NET_STATE]: local net.");
+		appendJs(BOOTSTRAP_JS_PATH);
+		appendJs(JQ_UI_JS_PATH);
+		let vuejs = appendJs(VUEJS_PATH);
 
-			vuejs.onload = () => {
-				appendJs(CUSTOM_VUEJS_PATH);
-			};
-		});
-	}
+		appendCss(BOOTSTRAP_CSS_PATH);
+		appendCss(JQ_UI_CSS_PATH);
+
+		vuejs.onload = () => {
+			appendJs(CUSTOM_VUEJS_PATH);
+		};
+	};
+
+	jqEl.onerror = () => {
+		let w_jqEl = appendJs(W_JQ_PATH);
+
+		//等待jqurey加载完毕
+		w_jqEl.onload = () => {
+			console.log("[NET_STATE]: online.");
+			$(function () {
+				appendJs(W_BOOTSTRAP_JS_PATH);
+				appendJs(W_JQ_UI_JS_PATH);
+				let vuejs = appendJs(W_VUEJS_PATH);
+
+				appendCss(W_BOOTSTRAP_CSS_PATH);
+				appendCss(W_JQ_UI_CSS_PATH);
+
+				vuejs.onload = () => {
+					appendJs(CUSTOM_VUEJS_PATH);
+				};
+			});
+		};
+	};
 
 };
 
 function appendJs(URL) {
-	console.log(URL);
+	console.log("[FILE_PATH]:" + URL);
 	var el = document.createElement('script');
+	el.type = 'text/javascript';
 	el.src = URL;
 	document.body.appendChild(el);
 
@@ -46,7 +81,7 @@ function appendJs(URL) {
 };
 
 function appendCss(URL) {
-	console.log(URL);
+	console.log("[FILE_PATH]:" + URL);
 	var el = document.createElement('link');
 	el.href = URL;
 	el.rel = 'stylesheet';
@@ -54,4 +89,3 @@ function appendCss(URL) {
 	// HEAD要素の最後に追加
 	document.getElementsByTagName('head')[0].appendChild(el);
 }
-
