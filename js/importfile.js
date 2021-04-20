33c91a661a4bb360e7363c751c8d7dd9a681694e
xxh200812345 <46435236+xxh200812345@users.noreply.github.com>
2021-04-20 11:50:27 +0900

    Update importfile.js

--- js/importfile.js
+++ js/importfile.js
@@ -24,6 +24,9 @@ const W_VUEJS_PATH = 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js';
 
 //主逻辑文件
 const CUSTOM_VUEJS_PATH = './js/dovue.js';
+//文字处理文件
+const CUSTOM_STRING_PATH = './js/string.js';
+
 
 //console.log = () => { };
 
@@ -42,6 +45,7 @@ window.onload = function () {
 		appendCss(JQ_UI_CSS_PATH);
 
 		vuejs.onload = () => {
+			appendJs(CUSTOM_STRING_PATH, 'module');
 			appendJs(CUSTOM_VUEJS_PATH, 'module');
 		};
 	};
